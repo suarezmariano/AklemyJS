4887768ffb2fc4694824da55025c34ba14c88632
Mariano Suarez <marianosuarez@hotmail.com>
2021-09-26 23:06:23 -0300

    Configuración de puertos e inicio de app

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const express = require("express");
-const app = express();
 const path = require("path");
 
+// initializations
+const app = express();
+
+// ports
+app.set("port", process.env.PORT || 3000);
+
+// starting the server
+app.listen(app.get("port"), () => {
+    console.log("Server Running on Port ", app.get("port"));
+});
+
+// public
 const publicFolderPath = path.resolve(__dirname,"./public");
 app.use(express.static(publicFolderPath));
 
+// settings
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+
+
+module.exports = app;
