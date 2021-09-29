155cbacc0ba5088a02530a71e4af157c26f951c9
Mariano Suarez <marianosuarez@hotmail.com>
2021-09-29 00:03:59 -0300

    Agregado de error code 404 page not found

--- app.js
+++ app.js
@@ -25,5 +25,9 @@ const indexRouter = require("./routes/index");
 
 app.use("/", indexRouter);
 
+// page not found
+app.use((req, res, next) => {
+    res.status(404).render("not-found");
+});
 
 module.exports = app;
